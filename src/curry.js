10590da452aae94e3b9b8104ff053832a3cd8c80
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-22 20:17:16 +0800

    First commit

--- src/curry.js
+++ src/curry.js
@@ -1,18 +1,42 @@
+function functionOfArity(func, arity) {
+    if (arity === 4) {
+        return (a, b, c, d) => {
+            return func(a, b, c, d);
+        };
+    } else if (arity === 3) {
+        return (a, b, c) => {
+            return func(a, b, c);
+        };
+    } else if (arity === 2) {
+        return (a, b) => {
+            return func(a, b);
+        };
+    } else if (arity === 1) {
+        return (a) => {
+            return func(a);
+        };
+    }
+}
+
 function curry(func) {
-    let numOfProvidedArguments = [];
+    let providedArguments = [];
     const numOfParametersInFunc = func.length;
 
     function curriedFunction(...args) {
-        numOfProvidedArguments = [...numOfProvidedArguments, ...args];
+        const passedInArguments = args.filter((arg) => arg !== undefined);
+        providedArguments = [...providedArguments, ...passedInArguments];
 
-        if (numOfProvidedArguments.length === numOfParametersInFunc) {
-            return func(...numOfProvidedArguments);
+        if (providedArguments.length === numOfParametersInFunc) {
+            return func(...providedArguments);
         }
 
-        return curriedFunction;
+        return functionOfArity(
+            curriedFunction,
+            numOfParametersInFunc - providedArguments.length
+        );
     }
 
-    return curriedFunction;
+    return functionOfArity(curriedFunction, numOfParametersInFunc);
 }
 
 export default curry;
--- src/curry.test.js
+++ src/curry.test.js
@@ -17,4 +17,25 @@ describe('curry', () => {
         expect(g(6, 2)).toBe(15);
         expect(mockedFunctionToCurry).toHaveBeenCalledWith(12, 3, 6, 2);
     });
+    it('allows further currying of a curried function', () => {
+        const mockedFunctionToCurry = jest.fn((a, b, c, d) => (a + b * c) / d);
+        const f = curry(mockedFunctionToCurry);
+        const g = f(12);
+        const h = g(3);
+
+        expect(h(6, 2)).toBe(15);
+        expect(mockedFunctionToCurry).toHaveBeenCalledWith(12, 3, 6, 2);
+    });
+    it('properly reports the length of the curried function', () => {
+        const mockedFunctionToCurry = jest.fn((a, b, c, d) => (a + b * c) / d);
+        const f = curry(mockedFunctionToCurry);
+        expect(f.length).toBe(4);
+
+        const g = f(12);
+        expect(g.length).toBe(3);
+        expect(g(3, 6).length).toBe(1);
+
+        const h = g(3);
+        expect(h.length).toBe(2);
+    });
 });
