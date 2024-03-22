3993748cba28037a02faecd9672e3bbacb3b03db
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-22 16:03:35 +0800

    RED: curry › curries multiple values

--- src/curry.test.js
+++ src/curry.test.js
@@ -1,11 +1,20 @@
-import { curry } from './curry';
+import curry from './curry';
 
 describe('curry', () => {
     it('curries a single value', () => {
-        const addFunction = (a, b, c, d) => (a + b * c) / d;
-        const f = curry(addFunction);
+        const mockedFunctionToCurry = jest.fn((a, b, c, d) => (a + b * c) / d);
+        const f = curry(mockedFunctionToCurry);
         const g = f(12);
 
         expect(g(3, 6, 2)).toBe(15);
+        expect(mockedFunctionToCurry).toHaveBeenCalledWith(12, 3, 6, 2);
+    });
+    it('curries multiple values', () => {
+        const mockedFunctionToCurry = jest.fn((a, b, c, d) => (a + b * c) / d);
+        const f = curry(mockedFunctionToCurry);
+        const g = f(12, 3);
+
+        expect(g(6, 2)).toBe(15);
+        expect(mockedFunctionToCurry).toHaveBeenCalledWith(12, 3, 6, 2);
     });
 });
