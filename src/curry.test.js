3a2934512f3c7f89499f87dbee57a20d40358330
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-27 15:34:38 +0800

    RED: curry › supports _ placeholder

--- src/curry.test.js
+++ src/curry.test.js
@@ -1,4 +1,5 @@
 import curry from './curry';
+import _ from './_';
 
 describe('curry', () => {
     it('curries a single value', () => {
@@ -43,14 +44,48 @@ describe('curry', () => {
         expect(typeof g(3, 6)).toEqual('function');
         expect(g(3, 6).length).toBe(1);
     });
-    it('preserves context', function () {
+    it('preserves context', () => {
         const ctx = { x: 10 };
-        const f = jest.fn(function (a, b) {
-            a + b * this.x;
-        });
+        const f = function (a, b) {
+            return a + b * this.x;
+        };
         const g = curry(f);
 
         expect(g.call(ctx, 2, 4)).toEqual(42);
         expect(g.call(ctx, 2).call(ctx, 4)).toEqual(42);
+    });
+    it('supports _ placeholder', function () {
+        const f = function (a, b, c) {
+            return [a, b, c];
+        };
+        const g = curry(f);
+
+        function eq(result, expected) {
+            return expect(result).toEqual(expected);
+        }
+
+        eq(g(1)(2)(3), [1, 2, 3]);
+        eq(g(1)(2, 3), [1, 2, 3]);
+        eq(g(1, 2)(3), [1, 2, 3]);
+        eq(g(1, 2, 3), [1, 2, 3]);
+
+        eq(g(_, 2, 3)(1), [1, 2, 3]);
+        eq(g(1, _, 3)(2), [1, 2, 3]);
+        eq(g(1, 2, _)(3), [1, 2, 3]);
+
+        eq(g(1, _, _)(2)(3), [1, 2, 3]);
+        eq(g(_, 2, _)(1)(3), [1, 2, 3]);
+        eq(g(_, _, 3)(1)(2), [1, 2, 3]);
+
+        eq(g(1, _, _)(2, 3), [1, 2, 3]);
+        eq(g(_, 2, _)(1, 3), [1, 2, 3]);
+        eq(g(_, _, 3)(1, 2), [1, 2, 3]);
+
+        eq(g(1, _, _)(_, 3)(2), [1, 2, 3]);
+        eq(g(_, 2, _)(_, 3)(1), [1, 2, 3]);
+        eq(g(_, _, 3)(_, 2)(1), [1, 2, 3]);
+
+        eq(g(_, _, _)(_, _)(_)(1, 2, 3), [1, 2, 3]);
+        eq(g(_, _, _)(1, _, _)(_, _)(2, _)(_)(3), [1, 2, 3]);
     });
 });
